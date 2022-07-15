57c2821a19c0d869e5c4193e4b4c1fd766223159
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2022-07-15 09:24:03 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="container">
+    <div>
+      <!--h1 class="title">
+        <a href="https://qiita.com/godan09/items/73015d0b8ef3a4dd8dba" target="_blank">Tutorial</a>
+      </h1-->
+      <lottie :options="defaultOptions" v-on:animCreated="handleAnimation"/>
+    </div>
+  </section>
+</template>
+
+<script>
+import Lottie from "~/components/Lottie.vue";
+import * as animationData from "~/assets/header.json";
+
+export default {
+  components: {
+    Lottie
+  },
+  data() {
+    return {
+      defaultOptions: { animationData: animationData },
+      animationSpeed: 1
+    };
+  },
+  methods: {
+    handleAnimation: function(anim) {
+      this.anim = anim;
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #002c3e;
+}
+a {
+  position: absolute;
+  color: #FFF;
+  top: 40px;
+  right: 40px;
+}
+</style>
